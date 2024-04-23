5b48978aca5b003656d36b2fef49e57d6d5b465c
cltubigon <cltubigon@gmail.com>
2024-04-23 17:16:40 +0800

    added share buttons

--- src/app/recipients/[path_url]/hugMessageShare/HugMessageShare.js
+++ src/app/recipients/[path_url]/hugMessageShare/HugMessageShare.js
@@ -1,52 +1,52 @@
 // import Image from 'next/image'
 // import Link from 'next/link'
-// import React from 'react'
+import React from 'react'
 // import heartMessage from '../images/heart-message.png'
 // import HugButtonContainer from './HugButtonContainer'
-// import ShareButton from './ShareButton'
+import ShareButton from './ShareButton'
 
-// const HugMessageShare = ({
-//   parameters: { id, path_url, firstName, hugs, package_image, sub_title },
-// }) => {
-//   return (
-//     <div className="py-[9px] w-full bg-[#E0F3FF]">
-//       <div
-//         className={
-//           'md:px-6 lg:px-10 xl:px-0 flex py-[26px] border-t-[1px] border-b-[1px] border-white'
-//         }
-//       >
-//         <div
-//           className={
-//             'container flex flex-col md:flex-row items-center md:items-[unset] justify-start md:justify-center gap-4 md:gap-6 lg:gap-[55px]'
-//           }
-//         >
-//           {/* ******** HugButtonContainer ******** */}
-//           <HugButtonContainer parameters={{ id, hugs, path_url }} />
-//           <Link href={`#comment-section`}>
-//             <div
-//               className={
-//                 'flex rounded-[10px] shadow-custom2 border-2 border-primary pt-2 px-[15px] pb-[6px] justify-center items-center bg-white cursor-pointer w-[176px]'
-//               }
-//             >
-//               <div className={'flex items-center gap-[13px]'}>
-//                 <p className={'text-[22px] font-mediumCond'}>Message</p>
-//                 <Image src={heartMessage} alt="care icon" quality={100} />
-//               </div>
-//             </div>
-//           </Link>
-//           {/* ******** Share Modal ******** */}
-//           <ShareButton
-//             parameters={{
-//               path_url,
-//               firstName,
-//               package_image,
-//               sub_title,
-//             }}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
+const HugMessageShare = ({
+  parameters: { id, path_url, firstName, hugs, package_image, sub_title },
+}) => {
+  return (
+    <div className="py-[9px] w-full bg-[#E0F3FF]">
+      <div
+        className={
+          'md:px-6 lg:px-10 xl:px-0 flex py-[26px] border-t-[1px] border-b-[1px] border-white'
+        }
+      >
+        <div
+          className={
+            'container flex flex-col md:flex-row items-center md:items-[unset] justify-start md:justify-center gap-4 md:gap-6 lg:gap-[55px]'
+          }
+        >
+          {/* ******** HugButtonContainer ******** */}
+          {/* <HugButtonContainer parameters={{ id, hugs, path_url }} />
+          <Link href={`#comment-section`}>
+            <div
+              className={
+                'flex rounded-[10px] shadow-custom2 border-2 border-primary pt-2 px-[15px] pb-[6px] justify-center items-center bg-white cursor-pointer w-[176px]'
+              }
+            >
+              <div className={'flex items-center gap-[13px]'}>
+                <p className={'text-[22px] font-mediumCond'}>Message</p>
+                <Image src={heartMessage} alt="care icon" quality={100} />
+              </div>
+            </div>
+          </Link> */}
+          {/* ******** Share Modal ******** */}
+          <ShareButton
+            parameters={{
+              path_url,
+              firstName,
+              package_image,
+              sub_title,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-// export default HugMessageShare
+export default HugMessageShare
--- src/app/recipients/[path_url]/hugMessageShare/ShareButton.js
+++ src/app/recipients/[path_url]/hugMessageShare/ShareButton.js
@@ -1,62 +1,62 @@
-// import Image from 'next/image'
-// import React, { useState } from 'react'
-// import returnIcon from '../images/return-icon.svg'
-// import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import returnIcon from '../images/return-icon.svg'
+import dynamic from 'next/dynamic'
 
-// const ShareModal = dynamic(() => import("./ShareModal"), { ssr: false })
+const ShareModal = dynamic(() => import("./ShareModal"), { ssr: false })
 
-// const ShareButton = ({
-//   parameters: { path_url, firstName, package_image, sub_title },
-// }) => {
-//   const [isModalActive, setisModalActive] = useState(false)
-//   const handleMouseLeave = ({ e, data }) => {
-//     if (data === 'button') {
-//       setisModalActive(false)
-//     }
-//   }
+const ShareButton = ({
+  parameters: { path_url, firstName, package_image, sub_title },
+}) => {
+  const [isModalActive, setisModalActive] = useState(false)
+  const handleMouseLeave = ({ e, data }) => {
+    if (data === 'button') {
+      setisModalActive(false)
+    }
+  }
 
-//   const handlebuttonHover = (e) => {
-//     e.stopPropagation()
-//     setisModalActive(true)
-//   }
+  const handlebuttonHover = (e) => {
+    e.stopPropagation()
+    setisModalActive(true)
+  }
 
-//   const handleClick = () => {
-//     console.log('handleClick')
-//     setisModalActive(() => !isModalActive)
-//   }
-//   const packageImage = package_image?.url
-//     ? `${package_image?.url}`
-//     : packageImageTemplate
-//   return (
-//     <div
-//       className={
-//         'flex rounded-[10px] shadow-custom2 border-2 border-primary pt-2 px-[15px] pb-[6px] justify-center items-center bg-white cursor-pointer w-[176px] relative'
-//       }
-//       onClick={(e) => handleClick(e)}
-//       onMouseOver={handlebuttonHover}
-//       onMouseLeave={(e) => handleMouseLeave({ e, data: 'button' })}
-//     >
-//       <div className={'flex items-center gap-[13px]'}>
-//         <p className={'text-[22px] font-mediumCond'}>Share page</p>
-//         <Image src={returnIcon} alt="care icon" quality={100} />
-//       </div>
-//       {isModalActive && (
-//         <div
-//           className="pb-[15px] absolute bottom-[49px] left-[-70px] md:left-[-30px]"
-//           onMouseLeave={handleMouseLeave}
-//         >
-//           <ShareModal
-//             parameters={{
-//               packageImage,
-//               path_url,
-//               firstName,
-//               sub_title,
-//             }}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
+  const handleClick = () => {
+    console.log('handleClick')
+    setisModalActive(() => !isModalActive)
+  }
+  const packageImage = package_image?.url
+    ? `${package_image?.url}`
+    : packageImageTemplate
+  return (
+    <div
+      className={
+        'flex rounded-[10px] shadow-custom2 border-2 border-primary pt-2 px-[15px] pb-[6px] justify-center items-center bg-white cursor-pointer w-[176px] relative'
+      }
+      onClick={(e) => handleClick(e)}
+      onMouseOver={handlebuttonHover}
+      onMouseLeave={(e) => handleMouseLeave({ e, data: 'button' })}
+    >
+      <div className={'flex items-center gap-[13px]'}>
+        <p className={'text-[22px] font-mediumCond'}>Share page</p>
+        <Image src={returnIcon} alt="care icon" quality={100} />
+      </div>
+      {isModalActive && (
+        <div
+          className="pb-[15px] absolute bottom-[49px] left-[-70px] md:left-[-30px]"
+          onMouseLeave={handleMouseLeave}
+        >
+          <ShareModal
+            parameters={{
+              packageImage,
+              path_url,
+              firstName,
+              sub_title,
+            }}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
 
-// export default ShareButton
+export default ShareButton
