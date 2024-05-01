f95df5a6d7d5d125c44cb8590dea26bcca4e5eaf
cltubigon <cltubigon@gmail.com>
2024-05-01 16:33:52 +0800

    made the navigation menu static

--- src/app/components/Navigation/ButtonLogin.js
+++ src/app/components/Navigation/ButtonLogin.js
@@ -1,23 +1,45 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
 import Button from '../Button'
-import { createServer } from '@/config/supabase/supabaseServer'
 import { logout } from '@/app/(auth)/signOut/actions'
+import { createClient } from '@/config/supabase/supabaseClient'
+import { useEffect, useState } from 'react'
 
-const ButtonLogin = async () => {
-  const supabase = createServer()
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser()
+const ButtonLogin = () => {
+  const [user, setuser] = useState(undefined)
+
+  const getUser = async () => {
+    const supabase = createClient()
+    const {
+      data: { user: theUser },
+      error,
+    } = await supabase.auth.getUser()
+    if (theUser) {
+      setuser(theUser)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   console.log('user', user)
   return (
     <>
-      {!user ? (
+      {/* Skeleton */}
+      {user === undefined && (
+        <Button className={'bg-gray-300 text-gray-300 animate-pulse'}>
+          Login
+        </Button>
+      )}
+      {/* Login */}
+      {user === null && (
         <Link href={'/login'}>
           <Button>Login</Button>
         </Link>
-      ) : (
+      )}
+      {/* Logout */}
+      {user && (
         <form>
           <Button type="submit" formAction={logout}>
             Log out
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import RecipientsPage from './recipients/page'
 
+export const revalidate = 30
+
 const Homepage = () => {
   return (
     <div>
