e17c9b2fe55ef1c9dc8ce7a7f26f4fb6f927bebd
cltubigon <cltubigon@gmail.com>
2024-05-05 23:51:30 +0800

    testing swiper

--- src/app/_homepage/recentRecipients/RecentRecipients.js
+++ src/app/_homepage/recentRecipients/RecentRecipients.js
@@ -1,19 +1,44 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import { listOfRecipients } from './listOfRecipients'
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
+// import Slider from 'react-slick'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
 const RecentRecipients = () => {
   return (
-    <div className="relative">
-      {listOfRecipients.map((item, i) => {
-        const { desc, imgUrl } = item
-        return (
-          <div key={i} className={''}>
-            <Image src={imgUrl} alt="recipient image" />
-            <p className={''}>{desc}</p>
-          </div>
-        )
-      })}
+    <div className={'container md:px-6 lg:px-10 xl:px-0'}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={20}
+        slidesPerView={4}
+        navigation
+        pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        className="flex"
+      >
+        {listOfRecipients.map((item, i) => {
+          const { desc, imgUrl } = item
+          return (
+            <SwiperSlide key={i}>
+              <div className="flex flex-col items-center bg-[#F6F6F6] w-fit px-[25px]">
+                <div className={'max-h-[225px] h-full'}>
+                  <Image src={imgUrl} alt="recipient image" />
+                </div>
+                <p className={'text-[18px] text-center'}>{desc}</p>
+              </div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
     </div>
   )
 }
