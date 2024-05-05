35ccc27be1d770ffe0ce0c58a9ee477438d7050e
cltubigon <cltubigon@gmail.com>
2024-05-05 19:27:13 +0800

    before installing swiper 2

--- src/app/_homepage/recentRecipients/listOfRecipients.js
+++ src/app/_homepage/recentRecipients/listOfRecipients.js
@@ -2,10 +2,42 @@ import adley from './images/adley-profile-pic-225x225.webp'
 import benny from './images/benny-sq-225x225.webp'
 import brenna from './images/Brenna2-225x225.webp'
 import sydney from './images/Sydney-225x225.webp'
+import miguel from './images/Mighty-Miguel2-225x225.webp'
+import sammy from './images/sammy2-225x225.webp'
+import brody from './images/Brody2-225x225.webp'
+import ryan from './images/Ryan-225x225.webp'
+import ayden from './images/Ayden-225x225.webp'
+import amy from './images/Amy-225x225.webp'
+import tink from './images/Tink-225x225.webp'
+import alyssa from './images/Alyssa-225x225.webp'
+import emma from './images/Emma-225x225.webp'
+import justin from './images/justin-225x225.webp'
+import david from './images/David3-220x225.webp'
+import Damian from './images/Damian-225x225.webp'
+import amanda from './images/Amanda-175x225.webp'
+import braiden from './images/Braiden2-205x225.webp'
+import marik from './images/Marik-225x225.webp'
+import mickey from './images/mickey-270x225.webp'
 
 export const listOfRecipients = [
   { desc: 'Adley is Battling Leukemia', imgUrl: adley },
   { desc: 'Benny is Battling Cancer', imgUrl: benny },
   { desc: 'Brenna is Battling DIPG (Brain Cancer)', imgUrl: brenna },
   { desc: 'Sydney is Battling Brain Cancer', imgUrl: sydney },
+  { desc: 'Miguel is Battling a Brain Tumor & Seizures', imgUrl: miguel },
+  { desc: 'Mickey is Battling Sanfilippo Syndrome', imgUrl: mickey },
+  { desc: 'Sammy is Battling Neuroblastoma', imgUrl: sammy },
+  { desc: 'Brody is 5 Years Cancer Free!', imgUrl: brody },
+  { desc: 'Ryan is Battling Pancreatic Disease', imgUrl: ryan },
+  { desc: 'Ayden is Battling Neuroblastoma Again', imgUrl: ayden },
+  { desc: 'Amy is Battling Serious Health Issues', imgUrl: amy },
+  { desc: 'Tink is Celebrating Her 6th Birthday!', imgUrl: tink },
+  { desc: 'Alyssa is Battling Neuroblastoma', imgUrl: alyssa },
+  { desc: 'Emma is Battling Leukemia Again', imgUrl: emma },
+  { desc: 'Justin is Battling Cancer Again', imgUrl: justin },
+  { desc: 'David Got A Kidney Transplant', imgUrl: david },
+  { desc: 'Damian is Battling Retinoblastoma Again', imgUrl: Damian },
+  { desc: 'Amanda is Battling Breast Cancer', imgUrl: amanda },
+  { desc: 'Braiden is Battling Neuroblastoma Again', imgUrl: braiden },
+  { desc: 'Marik is Battling Osteosarcoma Again', imgUrl: marik },
 ]
